7dceaa47c96e907f42a681174c4cee6ac52e865c
Noite <noite@F13.local>
2017-06-22 19:32:59 -0300

    Http server

---
+++ http_server/http-server/public/simpleWebServer.js
@@ -0,0 +1,69 @@
+//vamos criar uma variavel baseada no modulo de http
+var http = require("http");
+var circle = require("./module/circle.js");
+
+//vamos criar o nosso servidor
+var server = http.createServer(function(req,res) {
+	
+	//Vamos imprimir os parametros enviados na requisição
+	var url = require("url");
+	var params = url.parse(req.url, true);
+	//console.log(params.pathname);
+
+	var id = params.query.id;
+	var r = params.query.r;
+
+	if (r){
+		console.log("Area", circle.area(r));
+		console.log("Circumference", circle.circumference(r));
+	}
+
+	//baseando nos na url podemos indicar um arquivo para ser entregue para quem fez o request
+	//var fs = require("fs");
+	//fs.readFile(__dirname + params.pathname, function(err,contentReaded){
+
+		// if(err){
+		// 	res.writeHead(404, {'Content-Type': 'text/html',
+		// 						'Content-Type': 'charset=utf-8'}); 
+		// 	res.write("<h1>404 NOT FOUND ddsf</h1>");
+		// 	res.end();
+		// }
+
+		//estamos começando a escrever a resposta do meu servidor
+		res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'}); 
+		// res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'}); 
+		var obj = [];
+
+		var faker = require("faker");
+
+		if (params.pathname == "/guilherme" && id != 30){
+			obj.push({
+				id: 10,
+				nome: 'Guilherme Ventura'
+			});
+		}else if (id = 30){
+			obj.push({
+				id: 30,
+				nome: 'Teste id'
+			});
+		}else{
+			obj.push({
+				id: -1,
+				nome: 'Usuário não encontrado'
+			});
+		}
+		
+		// res.write(JSON.stringify(obj),"utf-8");
+		res.write("<h1>FAKER</h1>");
+		res.write("<h2>Nome: " + faker.name.findName() + "</h2>"); 
+		res.write("<img src='" + faker.image.avatar() + "'/>");
+		res.end();
+
+	// })
+
+});
+
+//starmos o nosso servidor
+server.listen(3000, function () {
+	console.log("Server STARTED port:3000");
+})
